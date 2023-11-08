3c76a659eb487f35f9c51015221e7c0a1308816b
David Damilola <dagbolade72@gmail.com>
2023-11-08 15:22:24 +0000

    continuing day 1.......

--- Day_1/day-1-printing-start.py
+++ Day_1/day-1-printing-start.py
@@ -2,4 +2,9 @@ print(" Hello World! \n Hello world! \n Hello World!")
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
-print("Hello " + "David")
+
+# input() will get user input in console
+print("Hello " + input("What is your name?") + "!")
+
+# get the length of the name
+print(len(input("What is your name?")))
