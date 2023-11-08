48b38f32b8d618e2508d6326c610eacbbea1e201
David Damilola <dagbolade72@gmail.com>
2023-11-08 17:07:58 +0000

    Also built a basic age predicto

---
+++ Day_1/Age_predictor.py
@@ -0,0 +1,16 @@
+# display a greeting message
+print("Welcome to the Age Predictor App")
+
+# get the user real name
+name = input("What is your name? \n")
+
+# get the user age
+
+age = int(input("What is your age? \n"))
+
+# calculate the age of the user after 5 years
+
+age_after_5_years = age + 5
+
+# display the age after 5 years
+print("Hello " + name + ", after 5 years you will be " + str(age_after_5_years) + " years old")
