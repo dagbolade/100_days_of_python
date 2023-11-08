5de028a1319d79a066bb98cf37791d62f0be46d2
David Damilola <dagbolade72@gmail.com>
2023-11-08 16:28:30 +0000

    BAND NAME GENERATOR PROJECT

--- Day_1/band-name-generator_project.py
+++ Day_1/band-name-generator_project.py
@@ -7,8 +7,13 @@ city_name = input("Whats the name of the city you grew up in? \n")
 # create a variable to store the pet name
 pet_name = input("Whats your pet's name? \n")
 
+#
+
 # create a variable to store the band name
 band_name = city_name + " " + pet_name
 
 # print the band name
 print("Your band name could be " + band_name)
+
+# end of the program
+
