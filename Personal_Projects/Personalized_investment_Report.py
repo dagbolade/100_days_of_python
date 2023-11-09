0b3c6b89dfac4777b5d536656ee69e91269ff163
David Damilola <dagbolade72@gmail.com>
2023-11-09 16:06:18 +0000

    Created a peronalized investment report project usisng the future value formular

---
+++ Personal_Projects/Personalized_investment_Report.py
@@ -0,0 +1,33 @@
+# Personalized investment Report Project
+# display a welcome message
+print("Welcome to the Personalized Investment Report App")
+
+# get the user real name
+name = input("What is your name? \n")
+
+# get the user age
+age = int(input("What is your age? \n"))
+
+# get the user initial investment, expected annual interest rate as a percentge and the number of years for the
+# investment.
+initial_investment = float(input("What is your initial investment? \n"))
+interest_rate = float(input("What is your expected annual interest rate? \n"))
+years = int(input("How many years will you be investing for? \n"))
+
+# calculate the future value of the investment
+# future_value = initial_investment * (1 + interest_rate / 100) ** years
+future_value = initial_investment * (1 + interest_rate / 100) ** years
+
+# generate the investment report
+print("Investment Report for " + name)
+# use a loop to calculate the value of the investment at the end of each year
+for i in range(1, years + 1):
+    print(f"Year {i}: £{future_value:.2f}")
+    future_value = future_value * (1 + interest_rate / 100)
+
+# present the starting conditions and a yearly report of the investment
+print(f"Initial Investment: £{initial_investment:.2f}")
+print(f"Annual Interest Rate: {interest_rate}%")
+print(f"Years Invested: {years}")
+print(f"Future Value: £{future_value:.2f}")
+print("Goodbye and Thank you!")
