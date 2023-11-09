be5a8a5e52fa640dfcc2da3fc28a7fe35e5d90bd
David Damilola <dagbolade72@gmail.com>
2023-11-09 14:04:23 +0000

    Type conversion

--- Day_2/type_error.py
+++ Day_2/type_error.py
@@ -1,10 +1,22 @@
-# Type Error, Type Checking, Type Conversion
-num_char = len(input("What is your name? "))
-# print("Your name has " + num_char + " characters.")
-# TypeError: can only concatenate str (not "int") to str
+# # Type Error, Type Checking, Type Conversion
+# num_char = len(input("What is your name? "))
+# # print("Your name has " + num_char + " characters.")
+# # TypeError: can only concatenate str (not "int") to str
+# # This error occurs because we are trying to concatenate a string with an integer.
+#
+# # Type Checking
+# print(type(num_char))
+#
+# # Type Conversion
+# # To fix this error, we need to convert the num_char variable into a string.
+# # We can do this by wrapping the variable with str().
+#
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
 
-# To fix this error, we need to convert the num_char variable into a string.
-# We can do this by wrapping the variable with str().
-
-# Type Checking
-print(type(num_char))
+a = 123
+print(type(a))
+b = str(a)
+print(type(b))
+c = float(a)
+print(type(c))
