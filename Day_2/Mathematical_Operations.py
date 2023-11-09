51903175deb545d7a542d36faf03e603a74804c0
David Damilola <dagbolade72@gmail.com>
2023-11-09 14:56:19 +0000

    calculated the bmi

--- Day_2/Mathematical_Operations.py
+++ Day_2/Mathematical_Operations.py
@@ -1,2 +1,21 @@
 #
 print(3 * 3 + 3 / 3 - 3)
+
+print(3 * (3 + 3) / 3 - 3)
+
+#exponent
+print(2 ** 2)
+
+#xeercise 2.1
+# Instructions
+# bmi calculator
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+bmi = int(weight) / float(height) ** 2
+
+print(int(bmi))
+
+#exercise 2.2
+# Instructions
+# Your life in weeks
