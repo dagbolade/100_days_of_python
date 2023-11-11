e46514979f1b6fc08a7095b53b8eeed93e29ec89
David Damilola <dagbolade72@gmail.com>
2023-11-11 04:03:21 +0000

    completed the treasure island game.....

--- Personal_Projects/Treasure_island.py
+++ Personal_Projects/Treasure_island.py
@@ -8,10 +8,12 @@ left_or_right = input("You're at a cross road. Where do you want to go? Type 'le
 # if the user chooses left, ask them to choose between swim or wait
 
 if left_or_right == "left":
-    swim_or_wait = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ").lower()
+    swim_or_wait = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait "
+                         "for a boat. Type 'swim' to swim across. ").lower()
     # if the user chooses wait, ask them to choose between red, blue or yellow
     if swim_or_wait == "wait":
-        red_blue_yellow = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ").lower()
+        red_blue_yellow = input("You arrive at the island unharmed. There is a house with 3 doors. One red, "
+                                "one yellow and one blue. Which colour do you choose? ").lower()
         # if the user chooses red, print "Burned by fire. Game Over."
         if red_blue_yellow == "red":
             print("Burned by fire. Game Over.")
