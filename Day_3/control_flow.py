95e84f419ceaef5a3195571679ee2be521b28f04
David Damilola <dagbolade72@gmail.com>
2023-11-11 01:13:56 +0000

    wrote a program that checks if number is an even or odd number

--- Day_3/control_flow.py
+++ Day_3/control_flow.py
@@ -1,8 +1,25 @@
-print("Welcome to the rollercoaster!")
-# convert input to integer
-height = int(input("What is your height in cm?"))
+# print("Welcome to the rollercoaster!")
+# # convert input to integer
+# height = int(input("What is your height in cm?"))
+#
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+#
+#
 
-if height > 120:
-    print("You can ride the rollercoaster!")
+# # Odd or Even
+# # Instructions
+# # Write a program that works out whether if a given number is an odd or even number.
+#
+# # Even numbers can be divided by 2 with no remainder.
+# Which number do you want to check?
+number = int(input())
+# 🚨 Don't change the code above 👆
+x = number % 2
+# Write your code below this line 👇
+if x == 0 :
+  print("This is an even number.")
 else:
-    print("Sorry, you have to grow taller before you can ride.")
+  print("This is an odd number.")
