5839583d5d8bec1b9bbf8027b5e79bf686f85323
David Damilola <dagbolade72@gmail.com>
2023-11-11 03:48:57 +0000

    statted the treasure island game

---
+++ Personal_Projects/Treasure_island.py
@@ -0,0 +1,6 @@
+# Treasure Island Game
+# Display a welcome message
+print("Welcome to Treasure Island.")
+# Display a welcome message
+print("Your mission is to find the treasure.")
+# Ask the user to choose between left or right
