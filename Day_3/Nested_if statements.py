84606f23966cda3f78f3298c98356c9ddd2a8432
David Damilola <dagbolade72@gmail.com>
2023-11-11 01:25:21 +0000

    Nested and elif statements

---
+++ Day_3/Nested_if statements.py
@@ -0,0 +1,18 @@
+# Nested if else statements
+print("Welcome to the rollercoaster!")
+# convert input to integer
+height = int(input("What is your height in cm?"))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    # Nested if else statements
+    age = int(input("What is your age?"))
+    if age < 12:
+        print("please pay $5")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
+
+else:
+    print("Sorry, you have to grow taller before you can ride.")
