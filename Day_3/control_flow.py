bc689bd62064c59be1b5c2ddb7413c325915b1a2
David Damilola <dagbolade72@gmail.com>
2023-11-11 00:58:51 +0000

    Day 3- started... control flow, with if else

---
+++ Day_3/control_flow.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+# convert input to integer
+height = int(input("What is your height in cm?"))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
