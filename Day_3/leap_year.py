e80e7b4eb2721785d4108ac87eed343df72e0ef8
David Damilola <dagbolade72@gmail.com>
2023-11-11 02:07:58 +0000

    Wrote a program to check if a year ois leap year or not

--- Day_3/leap_year.py
+++ Day_3/leap_year.py
@@ -7,12 +7,12 @@ year = int(input("Which year do you want to check?"))
 
 # Write your code below this line 👇
 if year % 4 == 0:
-  if year % 100 == 0:
-    if year % 400 == 0:
-      print("Leap year")
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year")
     else:
-      print("Not leap year")
-  else:
-    print("Leap year")
+        print("Leap year")
 else:
-  print("Not leap year")
+    print("Not leap year")
