756745a3db495a8972b68d7fec6237d74c8721e4
David Damilola <dagbolade72@gmail.com>
2023-11-12 23:14:57 +0000

    wrote code to show user a virtual coin toss

---
+++ Day_4/Heads_or_tail.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+heads_or_tail = random.randint(0,1)
+if heads_or_tail == 1:
+  print("Heads")
+else:
+  print("Tails")
