a0fa9db569b0c23f1a9dbeff413f61a8fb68bb36
David Damilola <dagbolade72@gmail.com>
2023-11-12 23:04:01 +0000

    Started Day 4....

--- Day_4/day_4.py
+++ Day_4/day_4.py
@@ -1,6 +1,12 @@
 # import random
 import random
-
+import my_module
 
 random_integer = random.randint(1, 10)
 print(random_integer)
+
+#print(my_module.pi)
+
+#returrn random floating numbers
+random_float = random.random() * 5
+print(random_float)
