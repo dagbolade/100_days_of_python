61ca59b02485d08bde38ba14edabbeb444f8898e
David Damilola <dagbolade72@gmail.com>
2023-11-13 00:28:38 +0000

    Treasure map...p2

--- Day_4/Treasure_map.py
+++ Day_4/Treasure_map.py
@@ -41,3 +41,23 @@ horizontal = int(position[0])
 vertical = int(position[1])
 map[vertical - 1][horizontal - 1] = "X"
 print(f"{line1}\n{line2}\n{line3}")
+
+# or
+
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input("Where do you want to put the treasure?") # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+letter = position[0].lower()
+abc = ["a", "b", "c"]
+letter_index = abc.index(letter)
+number_index = int(position[1]) - 1
+map[number_index][letter_index] = "X"
+
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
