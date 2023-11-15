6e6566744efaa47e88c787400f6fd4693ff5f462
David Damilola <dagbolade72@gmail.com>
2023-11-15 17:27:54 +0000

    Da6 6, practiced the reeborg world

--- Day_6/functions.py
+++ Day_6/functions.py
@@ -24,5 +24,105 @@ my_function()
 # for step in range(6):
 #     jump()
 
+# reeborgs world hurdle 2
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+# reeborgs world hurdle 3
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+#
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+
+#         move()
+
+# reeborgs world hurdle 4
+# def turn_right():
+
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+#
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     elif right_is_clear():
+#         turn_right()
+#         move()
+#     else:
+#         move()
+
+# reeborgs world Escaping the maze
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+
+
+
+
+
 
 
