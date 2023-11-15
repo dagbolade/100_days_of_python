4c5168b3f30f4f6988b986fd858ecbb0a2981654
David Damilola <dagbolade72@gmail.com>
2023-11-15 16:19:38 +0000

    Da6 6, practiced the reeborg worlf function game

--- Day_5/adding_even.py
+++ Day_5/adding_even.py
@@ -5,8 +5,8 @@ for number in range(2, target + 1, 2):
     total += number
 print(total)
 
-
 # or
+
 alt_total = 0
 for number in range(1, target + 1):
     if number % 2 == 0:
---
+++ Day_6/functions.py
@@ -0,0 +1,28 @@
+# Day 6: Functions
+def my_function():
+    print("Hello from a function")
+
+
+my_function()
+# reeborgs world
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+
+# for step in range(6):
+#     jump()
+
+
+
