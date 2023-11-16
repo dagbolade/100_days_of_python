423e48a44b39ba510b47810acb89079a8f2fe966
David Damilola <dagbolade72@gmail.com>
2023-11-16 00:02:06 +0000

    Created a basic calculator, just playing around if else

---
+++ Personal_Projects/Basic_calculator.py
@@ -0,0 +1,39 @@
+# Creating a basic calculator using python
+
+# display greetings
+print("Welcome to the basic calculator")
+name = input("Enter your name: ")
+print("Hello " + name + ", please enter the following details")
+
+# get the first number
+num1 = int(input("Enter the first number you wish to calculate: "))
+# get the second number
+num2 = int(input("Enter the second number you wish to calculate: "))
+
+# get the operator
+
+operator = input("Enter the operator you wish to use +, -, * or X, / : ")
+
+# calculate the result
+
+if operator == "+":
+    result = num1 + num2
+    # display the result
+    print("Hi, " + name + " The result is " + str(result))
+elif operator == "-":
+    result = num1 - num2
+    # display the result
+    print("Hi, " + name + " The result is " + str(result))
+elif operator == "*" or "X":
+    result = num1 * num2
+    # display the result
+    print("Hi, " + name + " The result is " + str(result))
+elif operator == "/":
+    result = num1 / num2
+    # display the result
+    print("Hi, " + name + " The result is " + str(result))
+else:
+    print("Invalid operator")
+
+
+
