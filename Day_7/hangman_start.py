3bc84e5739414942fcb029d709d6c551c90d623f
David Damilola <dagbolade72@gmail.com>
2023-11-16 19:52:00 +0000

    made it more interesting by adding feedback and alos creating a logo for the hangman

--- Day_7/hangman_start.py
+++ Day_7/hangman_start.py
@@ -1,68 +1,9 @@
 # step 1
-
-word_list = ["aardvark", "baboon", "camel"]
+import random
+from hangman_words import word_list
+from hangman_art import stages, logo
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-import random
-
-stages = ['''
-    +---+
-    |   |
-    O   |
-   /|\  |
-   / \  |
-        |
-=========
-''', '''
-    +---+
-    |   |
-    O   |
-   /|\  |
-   /    |
-        |
-=========
-''', '''
-    +---+
-    |   |
-    O   |
-   /|\  |
-        |
-        |
-=========
-''', '''
-    +---+
-    |   |
-    O   |
-   /|   |
-        |
-        |
-=========
-''', '''
-    +---+
-    |   |
-    O   |
-    |   |
-        |
-        |
-=========
-''', '''
-    +---+
-    |   |
-    O   |
-        |
-        |
-        |
-=========
-''', '''
-    +---+
-    |   |
-        |
-        |
-        |
-        |
-=========
-
-''']
 
 chosen_word = random.choice(word_list)
 
@@ -71,6 +12,8 @@ chosen_word = random.choice(word_list)
 lives = 6
 print(f"lives: {lives}")
 
+print("Welcome to Hangman, a game of guessing the word")
+print(logo)
 # TODO-3: - Create an empty List called display.
 # For each letter in the chosen_word, add a "_" to 'display'.
 # So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to
@@ -87,6 +30,10 @@ while "_" in display:
     # TODO-5: - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
     guess = input("Guess a letter: ").lower()
 
+    # TODO 5.1: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess in display:
+        print(f"You have already guessed {guess}, try another letter love")
+
     # TODO-6: - Loop through each position in the chosen_word;
     # If the letter at that position matches 'guess' then reveal that letter in the display at that position.
     # e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
@@ -98,9 +45,14 @@ while "_" in display:
     # Then reduce 'lives' by 1.
     # If lives goes down to 0 then the game should stop and it should print "You lose."
     if guess not in chosen_word:
+
+        # TODO 7.1: - If guess is not a letter in the chosen_word, print out the letter and let them know it's not in
+        #  the word.
+        print(f"{guess} is not in the word, you lose a life")
         lives -= 1
         if lives == 0:
-            print("You lose")
+            print("You lose, game over ")
+            print(f"The word was {chosen_word}")
             break
         print(stages[lives])
 
@@ -110,7 +62,7 @@ while "_" in display:
 
     # Check if user has got all letters.
     if "_" not in display:
-        print("You win")
+        print("You win!, you guessed the word correctly")
         break
 
     # TODO-8: - Print 'display' and you should see the guessed letter in the correct position and every other letter
@@ -120,5 +72,5 @@ while "_" in display:
     # TODO-9: - Print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has
     # remaining.
     # e.g. If the user has 5 lives left, print the 5th stage ASCII art.
-    print(stages[lives])
+
     print(f"lives: {lives}")
