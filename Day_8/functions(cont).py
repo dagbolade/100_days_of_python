cb08cbaf0404a6a1689fc50d35ed2def9ad4a096
David Damilola <dagbolade72@gmail.com>
2023-11-19 14:31:50 +0000

    keyword arguments

--- Day_8/functions(cont).py
+++ Day_8/functions(cont).py
@@ -1,7 +1,38 @@
-def greet():
-    print("Hi  David")
-    print("Welcome aboard")
-    print("nice to meet you")
+# def greet():
+#     print("Hi  David")
+#     print("Welcome aboard")
+#     print("nice to meet you")
+#
+#
+# greet()
+#
+#
+# # Functions with parameters
+#
+# def greet(name):
+#     print(f"Hi {name}")
+#     print("Welcome aboard")
+#     print(f"nice to meet you {name}")
+#
+#
+# greet("David")
 
-greet()
+# Functions with multiple parameters
+def greet(name, location):
+    print(f"Hi {name}")
+    print(f"Welcome aboard {name}")
+    print(f"you are from {location}")
 
+
+greet("David", "London")
+
+
+# keyword arguments
+
+def greet(name, location):
+    print(f"Hi {name}")
+    print(f"Welcome aboard {name}")
+    print(f"you are from {location}")
+
+
+greet(name=" Dami ", location="London")
