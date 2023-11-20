d7e44140c212c347039343c771d171643816988e
David Damilola <dagbolade72@gmail.com>
2023-11-20 18:03:40 +0000

    starred day 9 with dictionaries, completed the second excersise

--- Day_9/Dictionary.py
+++ Day_9/Dictionary.py
@@ -40,4 +40,23 @@ travel_log = {
     "Germany": ["Berlin", "Hamburg", "Stuttgart"],
 }
 
+# Nesting Dictionary in a Dictionary
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
+
+# Nesting Dictionary in a List
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5,
+    },
+]
 
