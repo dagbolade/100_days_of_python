6b9701c0dc1303b12b18d9bd3c850aed9ae90063
David Damilola <dagbolade72@gmail.com>
2023-11-22 12:56:38 +0000

    completed first exercise

--- Day_10/functions_output.py
+++ Day_10/functions_output.py
@@ -14,3 +14,16 @@ def format_name(f_name, l_name):
 
 
 print(format_name("angela", "yu"))
+
+
+# function with outputs
+# return statement
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs"
+    name1 = f_name.title()
+    name2 = l_name.title()
+    return f"{name1} {name2}"
+
+
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
