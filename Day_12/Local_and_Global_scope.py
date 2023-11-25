d78591aa62562ab4ea9234e5b8adcbbc2f76bc4d
David Damilola <dagbolade72@gmail.com>
2023-11-25 13:49:20 +0000

    sTARTED DAY 12

---
+++ Day_12/Local_and_Global_scope.py
@@ -0,0 +1,30 @@
+# Local Scope
+"""
+A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
+
+"""
+
+# Example
+# A variable created inside a function is available inside that function:
+def myfunc():
+  x = 300
+  print(x)
+
+myfunc()
+
+# Example
+# Function Inside Function
+"""
+As explained in the example above, the variable x is not available outside the function, but it is available for any function inside the function:
+
+"""
+
+# Example
+# The local variable can be accessed from a function within the function:
+def myfunc():
+  x = 300
+  def myinnerfunc():
+    print(x)
+  myinnerfunc()
+
+myfunc()
