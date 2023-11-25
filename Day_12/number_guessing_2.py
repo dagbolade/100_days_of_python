bf1f92053c3f8ac703a72a266ec9a6451ef5f821
David Damilola <dagbolade72@gmail.com>
2023-11-25 14:42:49 +0000

    another way to write the codes and make it better

--- Day_12/number_guessing_2.py
+++ Day_12/number_guessing_2.py
@@ -1,15 +1,20 @@
 import random
 from art import logo
+
 print(logo)
+
+
 def welcome_user():
     """Welcome the user and ask for their name."""
     name = input("What is your name? ").title()
     print(f"Hello {name}, I'm thinking of a number between 1 and 100.")
 
+
 def choose_difficulty():
     """Ask the user to choose a difficulty level."""
     level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
     return 10 if level == "easy" else 5 if level == "hard" else 0
+
 
 def play_game(chosen_number, attempts):
     """Play the Number Guessing Game."""
@@ -22,7 +27,9 @@ def play_game(chosen_number, attempts):
             attempts -= 1
             print("Too low." if guess < chosen_number else "Too high.")
             print("Guess again.")
-            print(f"You have {attempts} attempts remaining to guess the number.") if attempts > 0 else print(f"You've run out of guesses, you lose. The answer was {chosen_number}.")
+            print(f"You have {attempts} attempts remaining to guess the number.") if attempts > 0 else print(
+                f"You've run out of guesses, you lose. The answer was {chosen_number}.")
+
 
 def main():
     welcome_user()
@@ -31,6 +38,7 @@ def main():
     if attempts > 0:
         print(f"You have {attempts} attempts remaining to guess the number.")
         play_game(chosen_number, attempts)
+
 
 if __name__ == "__main__":
     main()
