f32508eeb93d59cd0d411be5071092c556e69477
David Damilola <dagbolade72@gmail.com>
2023-11-25 13:51:29 +0000

    LOCAL SCOPE AND GLOBAL SCOPE

--- Day_12/Local_and_Global_scope.py
+++ Day_12/Local_and_Global_scope.py
@@ -28,3 +28,20 @@ def myfunc():
   myinnerfunc()
 
 myfunc()
+
+# Global Scope
+"""
+A variable created in the main body of the Python code is a global variable and belongs to the global scope.
+
+Global variables are available from within any scope, global and local.
+    
+"""
+
+# Example
+# A variable created outside of a function is global and can be used by anyone:
+x = 300
+
+def myfunc():
+    print(x)
+
+myfunc()
