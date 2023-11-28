83b3a99f977d5b096fa7a355943790e034d31097
David Damilola <dagbolade72@gmail.com>
2023-11-28 23:20:29 +0000

    debugging.....

--- Day_13/Debug.py
+++ Day_13/Debug.py
@@ -25,6 +25,30 @@
 #     print("You are a Gen Z.")
 
 # Fix the Errors
-age = input("How old are you? ") # int(input("How old are you? "))
-if age > 18:
-    print(f"You can drive at age {age}.")
+# age = int(input("How old are you? ")) # int(input("How old are you? "))
+# if age > 18:
+#     print(f"You can drive at age {age}.") # wrong print("You can drive at age {age}.")
+
+# Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: ")) # == instead of =
+# # add a print statement to check the values of pages and word_per_page
+# print(pages, word_per_page)
+# total_words = pages * word_per_page
+# print(total_words)
+
+# Use a Debugger
+
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)  # b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1, 2, 3, 5, 8, 13])
+
+# Day 13 Project: Debugging Caesar Cipher
