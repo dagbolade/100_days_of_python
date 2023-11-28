f085f90953199cf359964bb022dc4bcb108a665e
David Damilola <dagbolade72@gmail.com>
2023-11-28 22:31:11 +0000

    day 13.... debugging

---
+++ Day_13/Debug.py
@@ -0,0 +1,10 @@
+# Describe the problem
+
+# Debugging
+# Describe Problem
+def my_function():
+    for i in range(1, 20):  # range(1, 20) -> range(1, 21)
+        if i == 20:
+            print("You got it")
+
+my_function()
