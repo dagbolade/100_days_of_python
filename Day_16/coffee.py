62739b0db83b2d6c7654d8728105dca6c6ba15a0
David Damilola <dagbolade72@gmail.com>
2023-12-17 20:28:52 +0000

    compleretd the oop coffee machine project

--- Day_16/coffee.py
+++ Day_16/coffee.py
@@ -1,3 +1,28 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+# create an object from the classes
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+# create a loop that asks the user what they want
+while True:
+    user = input(f"What would you like? ({menu.get_items()}): ").lower()
+    if user == "off":
+        break
+    elif user == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(user)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
+
+
+
