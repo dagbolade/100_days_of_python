512361737792238c61e752fb6f7c32aaf6f0b47d
David Damilola <dagbolade72@gmail.com>
2024-01-17 14:09:07 +0000

    started the quiz app

---
+++ Day_17/question_model.py
@@ -0,0 +1,11 @@
+# creating question class
+class Question:
+
+    # creating the constructor method
+    def __init__(self, question_text, question_answer):
+        # attributes
+        self.text = question_text
+        self.answer = question_answer
+
+
+
