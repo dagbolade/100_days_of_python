57ac7dd695f06261510dd0abc3428ed5b3343e18
David Damilola <dagbolade72@gmail.com>
2024-01-17 15:12:03 +0000

    completed the quiz app

--- Day_17/quiz_brain.py
+++ Day_17/quiz_brain.py
@@ -6,6 +6,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     # create a method to check if there are more questions
     def still_has_questions(self):
@@ -19,4 +20,18 @@ class QuizBrain:
         # increment the question number by 1
         self.question_number += 1
         # use the input function to show the question text and ask for the user's answer
-        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        # check if the user's answer is correct
+        self.check_answer(user_answer, current_question.answer)
+
+    # create a method to check if the user's answer is correct
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}.")
+        print("\n")
+
