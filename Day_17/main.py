2b09a882bb7ef66f20b3ac0ee08e7a6dada04add
David Damilola <dagbolade72@gmail.com>
2024-01-17 13:39:30 +0000

    started day17

--- Day_17/main.py
+++ Day_17/main.py
@@ -1,9 +1,36 @@
 # creating my very first class
 class User:
-    pass
+    # creating the constructor method which means that every time an object is created from this class,
+    # the constructor method will be called
+
+    def __init__(self, user_id, username):
+        # attributes
+        self.id = user_id
+        self.username = username
+        # adding a default value to an attribute
+        self.followers = 0
+        self.following = 0
+
+    # methods
+    def follow(self, user):
+        # the user we are following gets one more follower
+        user.followers += 1
+        # when we follow a user, we get one more following
+        self.following += 1
 
 
-user_1 = User()
-user_1.id = "001"
-user_1.username = "angela"
-print(user_1.username)
+# creating an object from the User class and passing in the arguments or parameters
+user_1 = User("001", "david")
+user_2 = User("002", "titi")
+
+print(user_1.id,user_1.username)
+print(user_2.id,user_2.username)
+
+# calling the follow method
+user_1.follow(user_2)
+user_2.follow(user_1)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
