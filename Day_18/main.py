57e0bb4dfdf26a5dfbd3ea84afc9f52e8ef4cbf2
David Damilola <dagbolade72@gmail.com>
2024-01-18 22:07:31 +0000

    completed the 3rd challenge

--- Day_18/main.py
+++ Day_18/main.py
@@ -9,7 +9,6 @@ tim.shape("turtle")
 # changing the color of the turtle
 tim.color("orchid")
 
-
 # timmy_the_turtle.forward(100)
 # timmy_the_turtle.right(90)
 # timmy_the_turtle.forward(100)
@@ -22,12 +21,62 @@ tim.color("orchid")
 #     tim.right(90)
 
 # draw a dashed line using a for loop and penup and pendown
-tim.color("dark orange")
-for times in range(15):
-    tim.forward(10) # move forward 10 pixels
-    tim.penup() # lift the pen up
-    tim.forward(10) # move forward 10 pixels
-    tim.pendown() # put the pen down
+# tim.color("dark orange")
+# for times in range(15):
+#     tim.forward(10) # move forward 10 pixels
+#     tim.penup() # lift the pen up
+#     tim.forward(10) # move forward 10 pixels
+#     tim.pendown() # put the pen down
+#
+
+# draw a triangle
+for times in range(3):
+    tim.color("dark slate blue")
+    tim.forward(100)
+    tim.right(120)
+
+# draw a square
+for times in range(4):
+    tim.color("dark turquoise")
+    tim.forward(100)
+    tim.right(90)
+
+# draw a pentagon
+for times in range(5):
+    tim.color("dark violet")
+    tim.forward(100)
+    tim.right(72)
+
+# draw a hexagon
+for times in range(6):
+    tim.color("dark olive green")
+    tim.forward(100)
+    tim.right(60)
+
+# draw a heptagon
+for times in range(7):
+    tim.color("dark khaki")
+    tim.forward(100)
+    tim.right(51.4285714)
+
+# draw a octagon
+for times in range(8):
+    tim.color("dark goldenrod")
+    tim.forward(100)
+    tim.right(45)
+
+# draw a nonagon
+for times in range(9):
+    tim.color("dark salmon")
+    tim.forward(100)
+    tim.right(40)
+
+# draw a decagon
+for times in range(10):
+    tim.color("dark slate gray")
+    tim.forward(100)
+    tim.right(36)
+
 
 
 # create a screen object for the turtle to draw on
