ed202463e01444ed07c4b677612ec8323037fa4d
David Damilola <dagbolade72@gmail.com>
2024-02-07 16:30:08 +0000

    created the scoreboard class

--- Day_22/ball.py
+++ Day_22/ball.py
@@ -8,14 +8,30 @@ class Ball(Turtle):  # creating the ball class
         self.shape("circle")
         self.color("white")
         self.penup()
-        self.x_move = 10
-        self.y_move = 10
+        self.x_move = 10  # to move the ball 10 pixels to the right
+        self.y_move = 10  # to move the ball 10 pixels up
 
 
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+        print(f"move: {self.x_move}")
+        print(f"move: {self.y_move}")
+
+
+    def bounce_y(self):
+        self.y_move *= -1 # to reverse the direction of the ball when it hits the wall
+        print(f"bounce_y: {self.y_move}")
+
+    def bounce_x(self):
+        self.x_move *= -1
+        # to reverse the direction of the ball when it hits the paddle
+        print(f"bounce_x: {self.x_move}")
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
 
 
 
---
+++ Day_22/scoreboard.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+# create the scoreboard class
+
+ALIGNMENT = "center"
+
+FONT = ("Courier", 80, "normal")
+
+
+class Scoreboard(Turtle):  # creating the scoreboard class
+    def __init__(self):
+        super().__init__()  # super() is used to call the __init__() method of the parent class which is Turtle
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(f"{self.l_score} ", align=ALIGNMENT, font=FONT)
+        self.goto(100, 200)
+        self.write(f"{self.r_score}", align=ALIGNMENT, font=FONT)
+
+    def l_point(self):
+        self.l_score += 1
+        self.clear()
+        self.update_score()
+
+    def r_point(self):
+        self.r_score += 1
+        self.clear()
+        self.update_score()
+
