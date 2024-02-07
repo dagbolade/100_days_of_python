fe7a25bcffdf963a91aab3d8f9325374b91dda9b
David Damilola <dagbolade72@gmail.com>
2024-02-07 18:41:19 +0000

    added an extra feature whch is playing against computer

--- Day_22/paddle.py
+++ Day_22/paddle.py
@@ -1,4 +1,5 @@
 # create paddle class
+from random import random
 from turtle import Turtle
 
 
@@ -18,6 +19,26 @@ class Paddle(Turtle): # creating the paddle class
     def go_down(self):
         new_y = self.ycor() - 20 # moving the paddle down
         self.goto(self.xcor(), new_y)
+
+        # Function to control the computer paddle
+    def computer_control(self, ball):
+        # Generate a random number between 0 and 1
+        random_number = random()
+
+        # Adjust this value to change the probability of the computer paddle moving
+        # Higher values make the computer paddle more likely to move
+        # Lower values make the computer paddle less likely to move
+        probability_threshold = 0.6
+
+        # If the random number is below the threshold, move the paddle up
+        if random_number < probability_threshold:
+            if ball.ycor() > self.ycor():
+                self.go_up()
+
+        # If the random number is above the threshold, move the paddle down
+        else:
+            if ball.ycor() < self.ycor():
+                self.go_down()
 
 
 
--- Day_22/pong.py
+++ Day_22/pong.py
@@ -17,15 +17,32 @@ l_paddle = Paddle((-350, 0))  # creating the left paddle
 ball = Ball()  # creating the ball
 scoreboard = Scoreboard()  # creating the scoreboard
 
-screen.listen()  # to listen to the keyboard inputs
+# to listen to the keyboard inputs
 
-# moving the right paddle
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
+# Prompt the user to choose the game mode
+game_mode = screen.textinput("Game Mode", "Choose game mode: '1' for vs. Computer or '2' for vs. Another Player")
 
-# moving the left paddle
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+# Assigning the paddle class to a variable
+if game_mode == "1":  # if the user selects 1, the game mode is vs. Computer
+    screen.listen()
+    #r_paddle = Paddle((350, 0))  # creating the right paddle in a tuple, because the position method in the paddle class
+    # l_paddle = Paddle((-350, 0))  # creating the left paddle
+    # Moving the right paddle
+    screen.onkey(r_paddle.go_up, "Up")
+    screen.onkey(r_paddle.go_down, "Down")
+elif game_mode == "2":  # if the user selects 2, the game mode is vs. Another Player
+    screen.listen()
+    r_paddle = Paddle((350, 0))  # creating the right paddle in a tuple, because the position method in the paddle class
+    l_paddle = Paddle((-350, 0))  # creating the left paddle
+    # Moving the right paddle
+    screen.onkey(r_paddle.go_up, "Up")
+    screen.onkey(r_paddle.go_down, "Down")
+    # Moving the left paddle
+    screen.onkey(l_paddle.go_up, "w")
+    screen.onkey(l_paddle.go_down, "s")
+else:
+    print("Invalid game mode selected. Please choose '1' or '2'.")
+    screen.bye()  # Close the window if an invalid mode is selected
 
 game_is_on = True
 while game_is_on:
@@ -36,6 +53,9 @@ while game_is_on:
     # detect collision with the wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()  # to bounce the ball when it hits the wall
+
+    # control the computer paddle
+    l_paddle.computer_control(ball)
 
     # detect collision with the paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
@@ -51,15 +71,11 @@ while game_is_on:
         ball.reset_position()  # to reset the position of the ball
         scoreboard.r_point()
 
-
-# when any of the score reaches 5, the game is over, the winner is displayed
+    # when any of the score reaches 5, the game is over, the winner is displayed
     if scoreboard.l_score == 5 or scoreboard.r_score == 5:
         print("Game Over, the winner is:" + " " + "Left Paddle" if scoreboard.l_score == 5 else "Right Paddle"
-              if scoreboard.r_score == 5 else "No winner")
+        if scoreboard.r_score == 5 else "No winner")
         game_is_on = False
         scoreboard.game_over()
-
-
-
 
 screen.exitonclick()  # to keep the window open until we click on it
